48f223e850c532eb49050b868673f824bf6ab030
Benjamin ter Kuile <bterkuile@gmail.com>
2013-09-14 11:49:25 +0200

    Allow dynamic finders using object belongs to associations

--- lib/simply_stored/couch/find_by.rb
+++ lib/simply_stored/couch/find_by.rb
@@ -6,16 +6,25 @@ module SimplyStored
         raise_when_not_found = name.to_s.end_with?('!')
         name = name.to_s.chop.to_sym if raise_when_not_found
         keys = name.to_s.sub(/^find_by_/, "").split("_and_").map(&:to_sym)
+
+        # replace asociation assignments with their property values if possible
+        keys.each.with_index do |key, i|
+          if properties.find{|p| p.name == key}.is_a?(SimplyStored::Couch::BelongsTo::Property)
+            keys[i] = "#{keys[i]}_id"
+          end
+        end
+
         view_name = name.to_s.sub(/^find_/, "").to_sym
         view_keys = keys.length == 1 ? keys.first : keys
         without_deleted_view_name = "#{view_name}_withoutdeleted"
         without_deleted_view_keys = keys + [:deleted_at]
-        
+
+
         unless respond_to?(view_name)
           puts "Warning: Defining view #{self.name}##{view_name} with keys #{view_keys.inspect} at call time, please add it to the class body. (Called from #{caller[1]})"
           view(view_name, :key => view_keys)
         end
-        
+
         if !respond_to?(without_deleted_view_name) && soft_deleting_enabled?
           puts "Warning: Defining view #{self.name}##{without_deleted_view_name} with keys #{without_deleted_view_keys.inspect} at call time, please add it to the class body. (Called from #{caller[1]})"
           view(without_deleted_view_name, :key => without_deleted_view_keys)
@@ -26,9 +35,11 @@ module SimplyStored
               options = key_args.last.is_a?(Hash) ? key_args.pop : {}
               options.assert_valid_keys(:with_deleted)
               with_deleted = options.delete(:with_deleted)
-              
-              raise ArgumentError, "Too many or too few arguments, require #{keys.inspect}" unless keys.size == key_args.size            
-              
+
+              raise ArgumentError, "Too many or too few arguments, require #{keys.inspect}" unless keys.size == key_args.size
+
+              key_args.map!{|a| a.is_a?(SimplyStored::Couch) ? a.id : a}
+
               if soft_deleting_enabled? && !with_deleted
                 key_args = key_args + [nil] # deleted_at
                 result = database.view(send(without_deleted_view_name, :key => (key_args.size == 1 ? key_args.first : key_args), :limit => 1, :include_docs => true)).first
@@ -46,9 +57,11 @@ module SimplyStored
               options = key_args.last.is_a?(Hash) ? key_args.pop : {}
               options.assert_valid_keys(:with_deleted)
               with_deleted = options.delete(:with_deleted)
-              
-              raise ArgumentError, "Too many or too few arguments, require #{keys.inspect}" unless keys.size == key_args.size            
-              
+
+              raise ArgumentError, "Too many or too few arguments, require #{keys.inspect}" unless keys.size == key_args.size
+
+              key_args.map!{|a| a.is_a?(SimplyStored::Couch) ? a.id : a}
+
               if soft_deleting_enabled? && !with_deleted
                 key_args = key_args + [nil] # deleted_at
                 database.view(send(without_deleted_view_name, :key => (key_args.size == 1 ? key_args.first : key_args), :limit => 1, :include_docs => true)).first
@@ -60,7 +73,7 @@ module SimplyStored
           send(name, *args)
         end
       end
-      
+
       def _define_find_all_by(name, *args)
         raise_when_not_found = name.to_s.end_with?('!')
         name = name.to_s.chop.to_sym if raise_when_not_found
@@ -70,17 +83,17 @@ module SimplyStored
         view_keys = keys.length == 1 ? keys.first : keys
         without_deleted_view_name = "#{view_name}_withoutdeleted"
         without_deleted_view_keys = keys + [:deleted_at]
-        
+
         unless respond_to?(view_name)
           puts "Warning: Defining view #{self.name}##{view_name} with keys #{view_keys.inspect} at call time, please add it to the class body. (Called from #{caller[1]})"
           view(view_name, :key => view_keys)
         end
-        
+
         if !respond_to?(without_deleted_view_name) && soft_deleting_enabled?
           puts "Warning: Defining view #{self.name}##{without_deleted_view_name} with keys #{without_deleted_view_keys.inspect} at call time, please add it to the class body. (Called from #{caller[1]})"
           view(without_deleted_view_name, :key => without_deleted_view_keys)
         end
-        
+
         if raise_when_not_found
           (class << self; self end).instance_eval do
             define_method(:"#{name}!") do |*key_args|
@@ -91,7 +104,7 @@ module SimplyStored
                 options[:key] = key_args.first if key_args.size == 1
                 options[:key] = key_args if key_args.size > 1
                 options[:include_docs] = true
-                
+
                 raise ArgumentError, "Too many or too few arguments, require #{keys.inspect}" unless keys.size == key_args.size || options[:keys]
 
                 if soft_deleting_enabled? && !with_deleted
@@ -116,7 +129,7 @@ module SimplyStored
                 options[:key] = key_args.first if key_args.size == 1
                 options[:key] = key_args if key_args.size > 1
                 options[:include_docs] = true
-                
+
                 raise ArgumentError, "Too many or too few arguments, require #{keys.inspect}" unless keys.size == key_args.size || options[:keys]
 
                 if soft_deleting_enabled? && !with_deleted
@@ -131,24 +144,24 @@ module SimplyStored
           send(name, *args)
         end
       end
-      
+
       def _define_count_by(name, *args)
         keys = name.to_s.sub(/^count_by_/, "").split("_and_")
         view_name = name.to_s.sub(/^count_/, "").to_sym
         view_keys = keys.length == 1 ? keys.first : keys
         without_deleted_view_name = "#{view_name}_withoutdeleted"
         without_deleted_view_keys = keys + [:deleted_at]
-        
+
         unless respond_to?(view_name)
           puts "Warning: Defining view #{self.name}##{view_name} with keys #{view_keys.inspect} at call time, please add it to the class body. (Called from #{caller[1]})"
           view(view_name, :key => view_keys)
         end
-        
+
         if !respond_to?(without_deleted_view_name) && soft_deleting_enabled?
           puts "Warning: Defining view #{self.name}##{without_deleted_view_name} with keys #{without_deleted_view_keys.inspect} at call time, please add it to the class body. (Called from #{caller[1]})"
           view(without_deleted_view_name, :key => without_deleted_view_keys)
         end
-        
+
         (class << self; self end).instance_eval do
           define_method("#{name}") do |*key_args|
             options = key_args.last.is_a?(Hash) ? key_args.pop : {}
@@ -164,10 +177,10 @@ module SimplyStored
             else
               database.view(send(view_name, options))
             end
-            
+
           end
         end
-      
+
         send(name, *args)
       end
     end
--- test/finder_test.rb
+++ test/finder_test.rb
@@ -7,21 +7,21 @@ class FinderTest < Test::Unit::TestCase
       CouchPotato::Config.database_name = 'simply_stored_test'
       recreate_db
     end
- 
+
     context "with find(:all)" do
       setup do
         User.create(:title => "Mr.")
         User.create(:title => "Mrs.")
       end
-      
-      should "return all instances" do  
+
+      should "return all instances" do
         assert_equal 2, User.find(:all).size
       end
-      
+
       should "allow a limit" do
         assert_equal 1, User.find(:all, :limit => 1).size
       end
-      
+
       should "allow to order the results" do
         assert_not_equal User.find(:all).map(&:id), User.find(:all, :order => :desc).map(&:id)
         assert_equal User.find(:all).map(&:id).reverse, User.find(:all, :order => :desc).map(&:id)
@@ -32,48 +32,48 @@ class FinderTest < Test::Unit::TestCase
       should 'generate a default find_all view' do
         assert User.respond_to?(:all_documents)
       end
-      
+
       should 'return all the users when calling all' do
         User.create(:title => "Mr.")
         User.create(:title => "Mrs.")
         assert_equal 2, User.all.size
       end
     end
-    
+
     context "to find one instance" do
       should 'return one user when calling first' do
         user = User.create(:title => "Mr.")
         assert_equal user, User.first
       end
-      
+
       should 'understand the order' do
         assert_nothing_raised do
           User.first(:order => :desc)
         end
       end
-      
+
       should 'find the last as a reverse first' do
         User.expects(:find).with(:first, :order => :desc)
         User.last
       end
-      
+
       should 'return nil when no user found' do
         assert_nil User.first
       end
     end
-    
+
     context "when finding with just an identifier" do
       should "find just one instance" do
         user = User.create(:title => "Mr.")
         assert User.find(user.id).kind_of?(User)
       end
-      
+
       should 'raise an error when no record was found' do
         assert_raises(SimplyStored::RecordNotFound) do
           User.find('abc')
         end
       end
-      
+
       should 'tell you which class failed to load something' do
         exception = nil
         begin
@@ -83,13 +83,13 @@ class FinderTest < Test::Unit::TestCase
         end
         assert_equal "User could not be found with \"abc\"", exception.message
       end
-      
+
       should 'raise an error when nil was specified' do
         assert_raises(SimplyStored::Error) do
           User.find(nil)
         end
       end
-      
+
       should 'raise an error when the record was not of the expected type' do
         post = Post.create
         assert_raises(SimplyStored::RecordNotFound) do
@@ -97,43 +97,55 @@ class FinderTest < Test::Unit::TestCase
         end
       end
     end
-    
+
     context "with a find_by prefix" do
       setup do
         recreate_db
       end
-      
+
       should "create a view for the called finder" do
         User.find_by_name("joe")
         assert User.respond_to?(:by_name)
       end
-      
+
       should 'not create the view when it already exists' do
         User.expects(:view).never
         User.find_by_name_and_created_at("joe", 'foo')
       end
-      
+
       should "create a method to prevent future loops through method_missing" do
         assert !User.respond_to?(:find_by_title)
         User.find_by_title("Mr.")
         assert User.respond_to?(:find_by_title)
       end
-      
+
       should "call the generated view and return the result" do
         user = User.create(:homepage => "http://www.peritor.com", :title => "Mr.")
         assert_equal user, User.find_by_homepage("http://www.peritor.com")
       end
-      
+
       should 'find only one instance when using find_by' do
         User.create(:title => "Mr.")
         assert User.find_by_title("Mr.").is_a?(User)
       end
-      
+
+      should "find a record through an association" do
+        user = User.create(:title => "Mr.")
+        post = Post.create user: user
+        assert_equal post, Post.find_by_user(user)
+      end
+
+      should "find a record through an association that raises when not found" do
+        user = User.create(:title => "Mr.")
+        post = Post.create user: user
+        assert_equal post, Post.find_by_user!(user)
+      end
+
       should "raise an error if the parameters don't match" do
         assert_raise(ArgumentError) do
           User.find_by_title()
         end
-        
+
         assert_raise(ArgumentError) do
           User.find_by_title(1,2,3,4,5)
         end
@@ -169,34 +181,34 @@ class FinderTest < Test::Unit::TestCase
         end
       end
     end
-    
+
     context "with a find_all_by prefix" do
       should "create a view for the called finder" do
         User.find_all_by_name("joe")
         assert User.respond_to?(:by_name)
       end
-      
+
       should 'not create the view when it already exists' do
         User.expects(:view).never
         User.find_all_by_name_and_created_at("joe", "foo")
       end
-      
+
       should "create a method to prevent future loops through method_missing" do
         assert !User.respond_to?(:find_all_by_foo_attribute)
         User.find_all_by_foo_attribute("Mr.")
         assert User.respond_to?(:find_all_by_foo_attribute)
       end
-      
+
       should "call the generated view and return the result" do
         user = User.create(:homepage => "http://www.peritor.com", :title => "Mr.")
         assert_equal [user], User.find_all_by_homepage("http://www.peritor.com")
       end
-      
+
       should "return an emtpy array if none found" do
         recreate_db
         assert_equal [], User.find_all_by_title('Mr. Magoooo')
       end
-      
+
       should 'find all instances when using find_all_by' do
         User.create(:title => "Mr.")
         User.create(:title => "Mr.")
@@ -208,12 +220,12 @@ class FinderTest < Test::Unit::TestCase
         User.create :name => 'doe', :title => 'Mr.'
         assert_equal ['doe', 'john'], User.find_all_by_name(:keys => ['john', 'doe', 'jane']).map{|u| u.name}.sort
       end
-      
+
       should "raise an error if the parameters don't match" do
         assert_raise(ArgumentError) do
           User.find_all_by_title()
         end
-        
+
         assert_raise(ArgumentError) do
           User.find_all_by_title(1,2,3,4,5)
         end
@@ -251,7 +263,7 @@ class FinderTest < Test::Unit::TestCase
           User.find_all_by_title_and_homepage!("Mr.", "http://www.companytools.nl/")
         end
       end
-    end      
+    end
   end
 
 end
