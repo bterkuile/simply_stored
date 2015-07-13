8e8288b20ab64d62f1da7516731cd936f81b1f19
Benjamin ter Kuile <bterkuile@gmail.com>
2015-07-13 09:40:13 +0200

    Add a bit more specs

---
+++ spec/validation_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+#describe 'callbacks' do
+  #context "after_save" do
+
+    #it "raise_error on normal save" do
+      #expect{ Callbacker.new(name: 'Cally').save }.to raise_error StandardError
+    #end
+
+    #it "not raise error on save with false" do
+      #expect{ Callbacker.new(name: 'Cally').save(false) }.not_to raise_error
+    #end
+
+    #it "not raise error on save with validate: false" do
+      #expect{ Callbacker.new(name: 'Cally').save(validate: false) }.not_to raise_error
+    #end
+
+    #it "raise error on save with validate: true" do
+      #expect{ Callbacker.new(name: 'Cally').save(validate: true) }.to raise_error StandardError
+    #end
+  #end
+#end
